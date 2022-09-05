b11bdf2dcdfa38bc2fd7e983320a6a6dfe729798
gangadhar <gangadharneelam81@gmail.com>
2022-09-05 07:49:22 +0530

    checkin apigateway

---
+++ src/main/java/com/appgateway/AppgatewayApplication.java
@@ -0,0 +1,13 @@
+package com.appgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppgatewayApplication.class, args);
+	}
+
+}
